c60eb4348a183c058262e26fe05232f2f1bce806
Diya Dangol <diya.dangol15@gmail.com>
2023-09-21 22:37:45 +0545

    Login issue solved

--- src/Components/Admin/Login.jsx
+++ src/Components/Admin/Login.jsx
@@ -1,9 +1,11 @@
 import {useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+
 
 const Login =()=>{
-    const [login, setLogin] = useState({
+    const [userlogin, setUserlogin] = useState({
         email:"",
         password:""
     })
@@ -11,40 +13,44 @@ const Login =()=>{
     const handleChange=(e)=>{
         const name= e.target.name;
         const value= e.target.value;
-        setLogin({...login, [name]: value})
+        setUserlogin({...userlogin, [name]: value})
     }
     
     const handleSubmit=(e)=>{
         e.preventDefault();
-        // if(login.email && login.email && login.password=== login.confirmPassword){
-            console.log(login);
+        // if(userlogin.email && userlogin.email && userlogin.password=== userlogin.confirmPassword){
+            console.log(userlogin);
         // }
         const requestOptions ={
             method: "POST",
-            headers: {"Authorization": "secret_token"},
-            body: JSON.stringify(login)
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(userlogin)
         }
-        // aba run hunxa?
         fetch(`${import.meta.env.VITE_BASE_URL}/login`, requestOptions)
         .then(res=> res.json())
-        .then(data=>console.log(data))
+        // .then(data=>console.log(data))
     }
     return(
-        <div>
-            <Form className="form" onSubmit={handleSubmit}>
+        <div className="form">
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Your email </Form.Label>
-                    <Form.Control type="email" placeholder="Enter your email" name="email" value={login.email} onChange={handleChange}/>
+                    <Form.Control type="email" placeholder="Enter your email" name="email" value={userlogin.email} onChange={handleChange}/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter your Password" name="password" value={login.password} onChange={handleChange} />
+                    <Form.Control type="password" placeholder="Enter your Password" name="password" value={userlogin.password} onChange={handleChange} />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     login
                 </Button>
             </Form>
+            <Button variant="secondary">
+                <Link to="/register" variant="secondary">
+                    Register
+                </Link>
+            </Button>
         </div>
     )
 }
